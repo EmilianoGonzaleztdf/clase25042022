7b38ac57756eee9db73190366af7a3d4f84d807b
Emiliano Nicolas Gonzalez <103612852+EmilianoGonzaleztdf@users.noreply.github.com>
2022-05-02 22:34:52 -0300

    check OK tarea verificada (filmina 34/35) 1.1 tecnincas de programacion - algoritmos secuenciales.

--- src/datos.ts
+++ src/datos.ts
@@ -3,7 +3,5 @@ let dato = document.getElementById("datos");
 let rotulo = document.getElementById("rotulo");
 rotulo.innerHTML = "ingrese un dato:";
 btnEnv.addEventListener("click", () => {
-  // tu codigo va aqui, por ejemplo:
   console.log("el dato ingresado es", dato.value);
-  //
 });
