ad93daee850f536f1bfea2dd926f34f34095d2f0
Emiliano Nicolas Gonzalez <103612852+EmilianoGonzaleztdf@users.noreply.github.com>
2022-05-03 21:43:32 -0300

    correccion de codigo mejore mi codigo

--- src/datos.ts
+++ src/datos.ts
@@ -1,6 +1,6 @@
-let btnEnv = document.getElementById("btnEnviar");
 let dato = document.getElementById("datos");
 let rotulo = document.getElementById("rotulo");
+let btnEnv = document.getElementById("btnEnviar");
 rotulo.innerHTML = "Indique la altura de la persona:";
 btnEnv.addEventListener("click", () => {
   console.log("la altura es:", dato.value);
